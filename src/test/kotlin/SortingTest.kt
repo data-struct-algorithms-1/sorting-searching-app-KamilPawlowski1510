fd797e98a2c9e5870e26f54755a99c1516f11b4f
KamilPawlowski1510 <20102337@mail.wit.ie>
2024-03-07 12:18:21 +0000

    Complete week 3 JUnit tests

--- src/test/kotlin/SortingTest.kt
+++ src/test/kotlin/SortingTest.kt
@@ -1,7 +1,9 @@
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
+import utilities.bubbleSort
 import utilities.insertionSort
+import utilities.mergeSort
 import utilities.selectionSort
 
 class SortingTest {
@@ -209,6 +211,220 @@ class SortingTest {
 
             // Act
             insertionSort(array)
+
+            // Assert
+            assertEquals("a", array[0])
+            assertEquals("b", array[1])
+            assertEquals("f", array[2])
+            assertEquals("i", array[3])
+            assertEquals("q", array[4])
+            assertEquals("z", array[5])
+        }
+    }
+
+    @Nested
+    inner class BubbleSort {
+
+        @Test
+        fun `bubbleSort should work on an empty array`() {
+            // Arrange
+            val array = arrayOf<Int>()
+
+            // Act
+            bubbleSort(array)
+        }
+
+        @Test
+        fun `bubbleSort should work on an array of one`() {
+            // Arrange
+            val array = arrayOf<Int>(5)
+
+            // Act
+            bubbleSort(array)
+        }
+
+        @Test
+        fun `bubbleSort should work on a sorted array`() {
+            // Arrange
+            val array = arrayOf<Int>(-2, -1, 0, 1, 2, 3)
+
+            // Act
+            bubbleSort(array)
+
+            // Assert
+            assertEquals(-2, array[0])
+            assertEquals(-1, array[1])
+            assertEquals(0, array[2])
+            assertEquals(1, array[3])
+            assertEquals(2, array[4])
+            assertEquals(3, array[5])
+        }
+
+        @Test
+        fun `bubbleSort should work on a completely random array`() {
+            // Arrange
+            val array = arrayOf<Int>(7, 15, 460, -19, 2, -5)
+
+            // Act
+            bubbleSort(array)
+
+            // Assert
+            assertEquals(-19, array[0])
+            assertEquals(-5, array[1])
+            assertEquals(2, array[2])
+            assertEquals(7, array[3])
+            assertEquals(15, array[4])
+            assertEquals(460, array[5])
+        }
+
+        @Test
+        fun `bubbleSort should work on an array sorted in reverse order`() {
+            // Arrange
+            val array = arrayOf<Int>(3, 2, 1, 0, -1, -2)
+
+            // Act
+            bubbleSort(array)
+
+            // Assert
+            assertEquals(-2, array[0])
+            assertEquals(-1, array[1])
+            assertEquals(0, array[2])
+            assertEquals(1, array[3])
+            assertEquals(2, array[4])
+            assertEquals(3, array[5])
+        }
+
+        @Test
+        fun `bubbleSort should work on an array with multiple of the same value`() {
+            // Arrange
+            val array = arrayOf<Int>(1, 2, 3, 3, 3, 1)
+
+            // Act
+            bubbleSort(array)
+
+            // Assert
+            assertEquals(1, array[0])
+            assertEquals(1, array[1])
+            assertEquals(2, array[2])
+            assertEquals(3, array[3])
+            assertEquals(3, array[4])
+            assertEquals(3, array[5])
+        }
+
+        @Test
+        fun `bubbleSort should work on an array of strings`() {
+            // Arrange
+            val array = arrayOf<String>("z", "b", "q", "f", "i", "a")
+
+            // Act
+            bubbleSort(array)
+
+            // Assert
+            assertEquals("a", array[0])
+            assertEquals("b", array[1])
+            assertEquals("f", array[2])
+            assertEquals("i", array[3])
+            assertEquals("q", array[4])
+            assertEquals("z", array[5])
+        }
+    }
+
+    @Nested
+    inner class MergeSort {
+
+        @Test
+        fun `mergeSort should work on an empty array`() {
+            // Arrange
+            val array = arrayOf<Int>()
+
+            // Act
+            mergeSort(array)
+        }
+
+        @Test
+        fun `mergeSort should work on an array of one`() {
+            // Arrange
+            val array = arrayOf<Int>(5)
+
+            // Act
+            mergeSort(array)
+        }
+
+        @Test
+        fun `mergeSort should work on a sorted array`() {
+            // Arrange
+            val array = arrayOf<Int>(-2, -1, 0, 1, 2, 3)
+
+            // Act
+            mergeSort(array)
+
+            // Assert
+            assertEquals(-2, array[0])
+            assertEquals(-1, array[1])
+            assertEquals(0, array[2])
+            assertEquals(1, array[3])
+            assertEquals(2, array[4])
+            assertEquals(3, array[5])
+        }
+
+        @Test
+        fun `mergeSort should work on a completely random array`() {
+            // Arrange
+            val array = arrayOf<Int>(7, 15, 460, -19, 2, -5)
+
+            // Act
+            mergeSort(array)
+
+            // Assert
+            assertEquals(-19, array[0])
+            assertEquals(-5, array[1])
+            assertEquals(2, array[2])
+            assertEquals(7, array[3])
+            assertEquals(15, array[4])
+            assertEquals(460, array[5])
+        }
+
+        @Test
+        fun `mergeSort should work on an array sorted in reverse order`() {
+            // Arrange
+            val array = arrayOf<Int>(3, 2, 1, 0, -1, -2)
+
+            // Act
+            mergeSort(array)
+
+            // Assert
+            assertEquals(-2, array[0])
+            assertEquals(-1, array[1])
+            assertEquals(0, array[2])
+            assertEquals(1, array[3])
+            assertEquals(2, array[4])
+            assertEquals(3, array[5])
+        }
+
+        @Test
+        fun `mergeSort should work on an array with multiple of the same value`() {
+            // Arrange
+            val array = arrayOf<Int>(1, 2, 3, 3, 3, 1)
+
+            // Act
+            mergeSort(array)
+
+            // Assert
+            assertEquals(1, array[0])
+            assertEquals(1, array[1])
+            assertEquals(2, array[2])
+            assertEquals(3, array[3])
+            assertEquals(3, array[4])
+            assertEquals(3, array[5])
+        }
+
+        @Test
+        fun `mergeSort should work on an array of strings`() {
+            // Arrange
+            val array = arrayOf<String>("z", "b", "q", "f", "i", "a")
+
+            // Act
+            mergeSort(array)
 
             // Assert
             assertEquals("a", array[0])
